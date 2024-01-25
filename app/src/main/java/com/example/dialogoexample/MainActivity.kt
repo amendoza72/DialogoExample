a312d6b0c1b6e7b4cbca502fd0550f83faa61a70
Antonio Mendoza <antonio.mendoza@gmail.com>
2024-01-25 09:46:55 +0100

    DatePickerDialog

--- app/src/main/java/com/example/dialogoexample/MainActivity.kt
+++ app/src/main/java/com/example/dialogoexample/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.dialogoexample
 
+import android.app.DatePickerDialog
 import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.DatePicker
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
+
+    var calendario = Calendar.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -33,8 +38,6 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, mensaje, Toast.LENGTH_LONG).show()
         }
 
-
-
         val neutralListener = object:DialogInterface.OnClickListener{
             override fun onClick(p0: DialogInterface?, p1: Int) {
                 var mensaje = "Bóton neutro pulsado"
@@ -49,5 +52,27 @@ class MainActivity : AppCompatActivity() {
 
         /*var alertDialog = builder.create()
         alertDialog.show()*/
+    }
+
+
+    fun onDatePickerDialogButton(view: View){
+
+        val datePickerListener= object:DatePickerDialog.OnDateSetListener{
+            override fun onDateSet(p0: DatePicker?, year: Int, month: Int, day: Int) {
+
+                calendario.set(year, month, day)
+
+                val mensaje: String= calendario.get(Calendar.DAY_OF_MONTH).toString() + "/" + calendario.get(Calendar.MONTH).toString() + "/" + calendario.get(Calendar.YEAR).toString()
+                Toast.makeText(applicationContext, mensaje, Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+
+
+        var fechaDialog = DatePickerDialog(this, datePickerListener,
+            calendario.get(Calendar.YEAR), calendario.get(Calendar.MONTH), calendario.get(Calendar.DAY_OF_MONTH))
+
+        fechaDialog.show()
     }
 }
