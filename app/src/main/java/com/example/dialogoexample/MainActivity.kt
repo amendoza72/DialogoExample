0b7a50905935b5396c0acdb9c1d23d3df2b138f8
Antonio Mendoza <antonio.mendoza@gmail.com>
2024-01-25 09:10:10 +0100

    Toast y Alert Dialog

--- app/src/main/java/com/example/dialogoexample/MainActivity.kt
+++ app/src/main/java/com/example/dialogoexample/MainActivity.kt
@@ -1,11 +1,53 @@
 package com.example.dialogoexample
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onButtonToast(view: View) {
+        Toast.makeText(this, R.string.mensaje_toast, Toast.LENGTH_LONG).show()
+    }
+
+    fun onAlertDialogButton(view:View){
+        var builder: AlertDialog.Builder = AlertDialog.Builder(this)
+
+        builder.setNegativeButton("No"){view, p1 ->
+            var mensaje = "Bóton negativo pulsado"
+            Toast.makeText(this, mensaje, Toast.LENGTH_LONG).show()
+        }
+
+        builder.setMessage(R.string.mensaje_alert)
+        builder.setTitle(R.string.titulo_alert)
+
+        builder.setPositiveButton("Si"){view, p1 ->
+            var mensaje = "Bóton positivo pulsado"
+            Toast.makeText(this, mensaje, Toast.LENGTH_LONG).show()
+        }
+
+
+
+        val neutralListener = object:DialogInterface.OnClickListener{
+            override fun onClick(p0: DialogInterface?, p1: Int) {
+                var mensaje = "Bóton neutro pulsado"
+                Toast.makeText(applicationContext, mensaje, Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+        builder.setNeutralButton("Puede", neutralListener)
+
+        builder.show()
+
+        /*var alertDialog = builder.create()
+        alertDialog.show()*/
     }
 }
